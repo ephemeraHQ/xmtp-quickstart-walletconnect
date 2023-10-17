acc611d64eed5a92e1c5045e52cff3409698af5b
fabri <fguespe@gmail.com>
2023-10-17 14:57:21 -0300

    updated node

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
+import { WagmiConfig } from "wagmi";
+import { useEffect, useState } from "react";
+import InboxPage from "./InboxPage-hooks";
+import {
+  arbitrum,
+  avalanche,
+  bsc,
+  fantom,
+  gnosis,
+  mainnet,
+  optimism,
+  polygon,
+} from "wagmi/chains";
+
+const chains = [
+  mainnet,
+  polygon,
+  avalanche,
+  arbitrum,
+  bsc,
+  optimism,
+  gnosis,
+  fantom,
+];
+
+// 1. Get projectID at https://cloud.walletconnect.com
+
+const projectId =
+  process.env.REACT_APP_WALLETCONNECT || "026ac8812e46e03ede0fa590783d1242";
+
+const metadata = {
+  name: "Next Starter Template",
+  description: "A Next.js starter template with Web3Modal v3 + Wagmi",
+  url: "https://web3modal.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+createWeb3Modal({ wagmiConfig, projectId, chains });
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setReady(true);
+  }, []);
+  return (
+    <>
+      {ready ? (
+        <WagmiConfig config={wagmiConfig}>
+          <InboxPage />
+        </WagmiConfig>
+      ) : null}
+    </>
+  );
+}
